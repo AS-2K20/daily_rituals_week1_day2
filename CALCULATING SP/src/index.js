4ddd2db882d888f59e334628b39e47c5fef35324
AS-2K20 <amudhan.psgcas@gmail.com>
2020-09-04 08:10:47 +0530

    SP and Mid Point Basic Programs

---
+++ CALCULATING SP/src/index.js
@@ -0,0 +1,10 @@
+//A man buys a cycle for Rs. a and sells it at a loss of b%. What is the selling price of the cycle?
+// Write a Function to compute the roundof selling price.
+
+// Write a Function to return compute the roundof selling price.
+function find_SellingPrice(a,b){
+    var loss = Math.round((b*a)/100);
+    var sp = a - loss;
+    return sp;
+}
+//Use SpecRunner to check the Test Cases.
